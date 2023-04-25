a504eac1e0b12bfa91c54b3ff205465de49a4e6e
ArthurOReis <arthuroreis2005@gmail.com>
2023-04-25 17:42:20 -0300

    Adicionando try catch e cálculos

--- src/Trigonometria/Calculo.java
+++ src/Trigonometria/Calculo.java
@@ -1,17 +1,42 @@
 package Trigonometria;
 
 public class Calculo {
-    public void calculo(String tipoCalculo, Object hipotenusa, Object catetoOposto, Object catetoAdjacente){
+    public double calculo(String tipoCalculo, Object hipotenusa, Object catetoOposto, Object catetoAdjacente, int anguloInterno){
+        double resultado = 0;
+
         switch (tipoCalculo){
             case "Seno":
-                System.out.println("Seno");
+                double senoAngulo = Math.round(seno(anguloInterno) * 10/ 10.0);
+                double adjacenteDouble = ((int)catetoOposto * (int)hipotenusa)/senoAngulo;
+                resultado = adjacenteDouble;
                 break;
             case "Cosseno":
-                System.out.println("Cosseno");
+                double cossenhoAngulo = Math.round(cosseno(anguloInterno) * 10/ 10.0);
+                double opostoDouble = ((int)catetoAdjacente * (int)hipotenusa)/cossenhoAngulo;
+                resultado = opostoDouble;
                 break;
             case "Tangente":
-                System.out.println("Tangente");
+                double tangenteAngulo = Math.round(tangente(anguloInterno) * 10/ 10.0);
+                double hipotenusaDouble = ((int)catetoOposto * (int)catetoAdjacente)/tangenteAngulo;
+                resultado = hipotenusaDouble;
                 break;
         }
+        return resultado;
     }
+
+    public double seno(int angulo) {
+        double radianos = Math.toRadians(angulo);
+        return Math.sin(radianos);
+    }
+
+    public double cosseno(int angulo) {
+        double radianos = Math.toRadians(angulo);
+        return Math.cos(radianos);
+    }
+
+    public double tangente(int angulo) {
+        double radianos = Math.toRadians(angulo);
+        return Math.tan(radianos);
+    }
+
 }
--- src/Trigonometria/Trigonometria.java
+++ src/Trigonometria/Trigonometria.java
@@ -5,17 +5,20 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Trigonometria {
-    static Object hipotenusa, catetoOposto, catetoAdjacente;
+    static Object hipotenusa, catetoOposto, catetoAdjacente, valorMisterioso;
+    String resultadoTrigonomia;
     static int anguloInterno;
+    ArrayList<Integer> lista = new ArrayList<Integer>(); //Instanciando Objeto ArrayList
+
     public void inicio(){
         Random random = new Random(); //Instanciando Objeto random
-        ArrayList<Integer> lista = new ArrayList<Integer>(); //Instanciando Objeto ArrayList
 
         for (int i = 0; i< 3; i++){
             int valor = random.nextInt(1, 51); //Atribui "valor" a um número aleatório de um a cinquenta
             lista.add(valor); //Adiciona os valores na lista
         }
 
+        /*Atribuindo os três índex a cada lado de um triângulo e criando um angulo de 1 a 90 graus*/
         hipotenusa = lista.get(0);
         catetoOposto = lista.get(1);
         catetoAdjacente = lista.get(2);
@@ -36,7 +39,7 @@ public class Trigonometria {
         System.out.println("Hipotenusa: "+ hipotenusa);
         System.out.println("Cateto Oposto: "+ catetoOposto);
         System.out.println("Cateto Adjacente: "+ catetoAdjacente);
-        System.out.println("Ângulo Intero: "+ anguloInterno+"\n");
+        System.out.println("Ângulo Interno: "+ anguloInterno+"\n");
     }
 
     public void escolha(){
@@ -44,24 +47,33 @@ public class Trigonometria {
         Calculo c = new Calculo();
 
         String resposta;
-        String resultadoTrigonomia;
 
         System.out.println("Tendo em vista os dados acima, qual método (SOH-CAH-TOA) irá utilizar para descobrir uma das medidas?:\n");
-        System.out.println("DICA:\nSOH = Seno do ângulo interno/Cateto Oposto = Hipotenusa/?\nCosseno do ângulo interno/Cateto Adjacente = Hipotenusa/?\nTangente do ângulo interno/Cateto Oposto = Cateto Adjacente/?:");
+        System.out.println("DICA:\nSOH = Seno do ângulo interno/Cateto Oposto = Hipotenusa/?\nCAH = Cosseno do ângulo interno/Cateto Adjacente = Hipotenusa/?\nTOA = Tangente do ângulo interno/Cateto Oposto = Cateto Adjacente/?:");
         System.out.println("[1]SOH [2]CAH [3]TOA");
 
         try{
-            resposta = Integer.toString(scanner.nextInt());
+            resposta = scanner.nextLine();
             resultadoTrigonomia = trigonomia(resposta);
-            c.calculo(resultadoTrigonomia, hipotenusa, catetoOposto, catetoAdjacente);
+            double resultado = c.calculo(resultadoTrigonomia, hipotenusa, catetoOposto, catetoAdjacente, anguloInterno);
+
+            for (int i = 0; i < 3; i++){
+                valorMisterioso = lista.get(i);
+                if (valorMisterioso == "X"){
+                    valorMisterioso = resultado;
+                }
+            }
+
+            System.out.println("A resposta é "+valorMisterioso);
+
         } catch (Exception e){
-            System.out.println("Insira um valor válido!");
+            System.out.println("Insira um valor válido!\n");
             escolha();
         }
     }
 
     public String trigonomia(String calculo){
-        String realizarCalculo = null;
+        String realizarCalculo = calculo;
         switch (calculo){
             case "1":
                 realizarCalculo = "Seno";
